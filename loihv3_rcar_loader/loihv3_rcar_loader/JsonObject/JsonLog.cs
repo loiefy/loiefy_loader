dc01489d436520e13b5413f9654c8f7cf9a70916
loiefy <loi.efy@gmail.com>
2021-01-10 00:00:07 +0700

    first commit

---
+++ loihv3_rcar_loader/loihv3_rcar_loader/JsonObject/JsonLog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace loiefy_loader
+{
+    public class JsonLog
+    {
+        public string enable { get; set; }
+        public string path { get; set; }
+    }
+}
---
+++ loihv3_rcar_loader/loihv3_rcar_loader/JsonObject/Project.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace loiefy_loader
+{
+    public class Project
+    {
+        public string timeout { get; set; }
+        public JsonComSetting com { get; set; }
+        public JsonLog log { get; set; }
+        public List <JsonActionNode> sequence { get; set; }
+
+        public Project()
+        {
+            timeout = "3000";
+            com = new JsonComSetting();
+            log = new JsonLog();
+            sequence = new  List<JsonActionNode>();
+        }
+    }
+}
